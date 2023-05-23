548f7ca3360442493b1b67a957a2a09ae1151d05
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-05-23 16:16:04 +0200

    yes

---
+++ script.js
@@ -0,0 +1,27 @@
+let goat = "I am Eighton-Lee,";
+
+function greet(e){
+    console.log(goat +"Hello");
+}
+
+greet()
+
+let num1 = parseInt('4');
+let num2 = parseInt('5');
+
+function multiply(numb1,numb2){
+    console.log(num1 * num2)
+}
+
+multiply()
+
+let indexxx = 5
+
+function tableOfFive(indexx){
+    let func = indexxx * 5
+    return console.log(func/5)
+}
+
+tableOfFive()
+
+// Log is the same as print in other coding languages. Return is used for returning values from a function.
